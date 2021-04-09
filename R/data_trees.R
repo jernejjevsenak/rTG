cad804fd968de303556cd77ea5c4e934468036c6
jernejjevsenak <jernej.jevsenak@gmail.com>
2021-04-09 12:35:52 +0200

    Data documented

---
+++ R/data_trees.R
@@ -0,0 +1,31 @@
+#' data_trees
+#'
+#' A dataset with intra-seasonal radial tree growth data. It was collected in
+#' three different years, at three sites, each with six trees. Please see
+#' references for details.
+#'
+#' @format A data frame with 945 rows and 8 variables:
+#' \describe{
+#'   \item{Tissue}{XYLEM or PHLOEM}
+#'   \item{Species}{Fagus sylvatica (FASY), Picea abies (PIAB), Quercus pubescens (QUPE)}
+#'   \item{Site}{Panska reka (PAN), Karst (KRAS)}
+#'   \item{Year}{2011, 2017}
+#'   \item{Tree}{Tree ID indicators ranging from 1 to 6}
+#'   \item{Sample}{A consecutive number of collected sample}
+#'   \item{DY}{Day in a year}
+#'   \item{EWM}{The total number of formed cells / the total ring width}
+#' }
+#' @source Gričar, J., Prislan, P., De Luis, M., Gryc, V., Hacurová, J.,
+#' Vavrčík, H., Čufar, K., 2015. Plasticity in variation of xylem and phloem
+#' cell characteristics of Norway spruce under different local conditions.
+#' Frontiers in plant science 6, 730.
+#' Gričar, J., Zavadlav, S., Jyske, T., Lavrič, M., Laakso, T., Hafner, P.,
+#' Eler, K., Vodnik, D., 2019. Effect of soil water availability on intra-annual
+#' xylem and phloem formation and non-structural carbohydrate pools in stem of
+#' Quercus pubescens. Tree Physiol. 39, 222-233.
+#' Prislan, P., Gričar, J., Čufar, K., de Luis, M., Merela, M., Rossi, S., 2019.
+#' Growing season and radial growth predicted for Fagus sylvatica under climate
+#' change. Clim. Change 153, 181-197.
+#'
+#' @export
+"data_trees"
