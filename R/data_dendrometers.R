8bf60297468c420f52d562d917a959aa4b9102cb
jernejjevsenak <jernej.jevsenak@gmail.com>
2022-05-10 22:46:14 +0200

    dendrometer data is added

---
+++ R/data_dendrometers.R
@@ -0,0 +1,17 @@
+#' data_dendrometers
+#'
+#' A dataset with dendrometer data from sugar maple and black spruce from
+#' Simoncouche site, Canada.
+#'
+#' @format A data frame with 718 rows and 6 variables:
+#' \describe{
+#'   \item{site}{Simoncouche}
+#'   \item{species}{sugar maple and black spruce}
+#'   \item{tree}{tree identifier}
+#'   \item{year}{2017, 2020}
+#'   \item{doy}{day Of a Year}
+#'   \item{width}{width expressed in relative terms}
+#' }
+#' @source Data belongs to Sergio Rossi
+#' @export
+"data_dendrometers"
